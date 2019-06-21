7b4c4964f57c1700d100bf6481a7729290e9109a
Mariana-Perez <Mariana.Perez@ibm.com>
2019-06-21 19:33:13 -0400

    add home and about page with simple routing handle in app component

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//create a component using class
+class AboutPage extends React.Component {
+  render() {
+    return (
+    <>
+      <h2>About Page</h2>
+      <p>This uses React</p>
+    </>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//function to create component
+function HomePage() {
+  return (
+  <div className="jumbotron">
+    <h1>Pluralsight Administration</h1>
+    <p>React, Flux, and React Router for web apps</p>
+    <a href="/about">About</a>
+  </div>
+  );
+}
+
+export default HomePage;
