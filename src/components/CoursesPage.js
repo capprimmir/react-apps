b241a8323b49c0ab93c9b23b5fd78a3d12ad7725
Mariana-Perez <Mariana.Perez@ibm.com>
2019-06-22 09:31:26 -0400

    add Course page and display a table dinamically with course info

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -8,12 +8,37 @@ class CoursePage extends React.Component {
 
   componentDidMount() {
     //get courses from the api. when the call completes, store the array of courses in state
-    getCourses().then(courses => this.setState({ courses: courses}))
+    getCourses().then(courses => this.setState({ courses: courses}));
   }
 
   //method to tell what to display
   render() {
-    return <h2>Courses</h2>
+    return (
+      <>
+      <h2>Courses</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.courses.map(course => {
+            return (
+              <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      </>
+    );
   }
 }
 
