d9d7e4bbe48f6a31f1c2614c6105dcabcf3b21fd
Mariana-Perez <Mariana.Perez@ibm.com>
2019-06-22 10:59:09 -0400

    add react Route to navigate between pages

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+//use an alias to make it a shorter name (as)
+import { BrowserRouter as Router } from "react-router-dom";
 
-render(<App />, document.getElementById("root"));
+
+render(
+  <Router>
+    <App /> 
+  </Router>,
+  document.getElementById("root")
+);
