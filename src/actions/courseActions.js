2d0136ee93ea9bc124f8e312cd6beda2caa4e6ab
Mariana-Perez <Mariana.Perez@ibm.com>
2019-06-24 16:43:24 -0400

    implement flux to create first action to save a course

---
+++ src/actions/courseActions.js
@@ -0,0 +1,16 @@
+import dispatcher from '../appDispatcher';
+import * as courseApi from '../api/courseApi';
+import actionTypes from './actionTypes';
+
+
+//action creator 
+export function  saveCourse(course) {
+  return courseApi.saveCourse(course).then(savedCourse => {
+    // dispatcher will notify all stores about this action
+    dispatcher.dispatch({
+      //the action
+      actionType: actionTypes.CREATE_COURSE,
+      course: savedCourse
+    });
+  })
+}
