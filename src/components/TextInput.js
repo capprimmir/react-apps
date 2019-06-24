9fc444d77792c425a6be48bb817af2c172a09540
Mariana-Perez <Mariana.Perez@ibm.com>
2019-06-24 08:38:11 -0400

    add reusable text input with error handler and PropTypes

---
+++ src/components/TextInput.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function TextInput (props) {
+
+  //if there is an error in props it would be red and handled by bootstrap 
+  // has-error class
+  let wrapperClass = "form-group";
+  if (props.error.length > 0) {
+    wrapperClass += " has-error";
+  }
+
+  return (
+    <div className="formGroup">
+        <label htmlFor={props.id}>{props.id}</label>
+        <div className="field">
+          <input
+            id={props.id}
+            type="text"
+            onChange={props.onChange}
+            name={props.name}
+            className="form-control"
+            value={props.value}          />
+        </div>
+        {props.error && <div className="alert alert-danger">{props.error} </div>}
+    </div>
+  ); 
+}
+
+TextInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+  error: PropTypes.string
+};
+
+TextInput.defaultProps = {
+  error: ""
+};
+
+export default TextInput;
